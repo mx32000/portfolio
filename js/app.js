40851fafa137c67993f831c73f9cf78618563cd7
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-07-12 14:17:58 -0500

    cards taking from json

--- js/app.js
+++ js/app.js
@@ -2,10 +2,19 @@ const $projects = $(".projects");
 $.ajax("./json/projects.json")
 .then(data => {
     data.forEach(project => {
-        const $project = $("#projects");
-        const $title = $("<h2>").addClass("name").text(`${project.name}`);
-        const $description = $("<h3>").addClass("description").text(`${project.description}`);
-        $project.append($title).append($description);
+        const $project = $("<div>").addClass("card");
+        const $image = $("<img>").addClass("card-img-top").attr("src", `${project.image}`);
+        const $body = $("<div>").addClass("card-body");
+        const $title = $("<h5>").addClass("card-title").text(`${project.name}`);
+        const $description = $("<p>").addClass("card-text").text(`${project.description}`);
+        const $links = $("<div>").addClass("card-links");
+        const $github = $("<a>").addClass("btn btn-primary").attr("href", `${project.github}`).text("Code");
+        const $deployed = $("<a>").addClass("btn btn-primary").attr("href", `${project.deployed}`).text("Link");
+
+        $links.append($github, $deployed);
+        $body.append($title, $description, $links);
+        $project.append($image, $body);
+
         $projects.append($project);
     })
 })
